b34ec818929184257e64cad7f672b8064e8f21ae
Sebastian Farias <sebastian.h.farias@gmail.com>
2025-11-03 23:55:48 -0300

    Nuevo proyecto con APK Android

---
+++ tienda1/android/app/src/main/java/com/jyptrend/tienda/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jyptrend.tienda;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- tienda1/vite.config.js
+++ tienda1/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Tienda/', // 👈 muy importante para rutas correctas en GitHub Pages
-  build: {
-    outDir: '../Tienda', // 👈 esto coloca el build final en /Tienda
-    emptyOutDir: true
+export default defineConfig(({ mode }) => {
+  // 📦 Si estás compilando para Android (ej: npm run build:android)
+  const isAndroid = mode === 'android'
+
+  return {
+    plugins: [react()],
+    base: isAndroid ? './' : '/Tienda/', // ✅ base local para Capacitor, GitHub para web
+    build: {
+      outDir: isAndroid ? 'dist' : '../Tienda', // ✅ build correcto según destino
+      emptyOutDir: true,
+    },
   }
 })
